823f1f9bdc8935c1ef89311141ecf452fd9d7e4d
Giuseppe Meli <meligiuseppe92020@gmail.com>
2023-03-23 00:44:57 +0100

    exercise-1

---
+++ exercise-1-primitive-types-start.ts
@@ -0,0 +1,27 @@
+/**
+ * START: Follow the instructions below.
+ */
+
+// Type annotations added:
+
+let country: string = "Italy";
+
+let year: number | undefined = undefined;
+
+let averageTemperature: number = 23.6;
+
+let visited: boolean = true;
+
+let currency: null = null;
+
+// Corrected type annotations:
+
+let population: number = 47_450_795;
+
+let isSummer: boolean = false;
+
+let languages: string = "Spanish, English";
+
+// ----
+
+export {};
